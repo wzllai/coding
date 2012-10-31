38ff9fe0dac6694e5ed71e2ae30b56e0a06fc924
@com <funyq@foxmail.com>
2012-10-31 10:53:20 +0800

    add  啊phpunitSuit

---
+++ phpunitSuit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 单元测试套件，执行以Test.php结尾的文件
+ * 运行全部测试 直接执行  phpunit Suite.php
+ * 运行指定模块(可多个)测试 执行  phpunit Suite.php 模块名 [模块名 ...]
+ */
+
+require_once "PHPUnit/Framework.php";
+
+class Suite extends PHPUnit_Framework_TestSuite
+{
+	public function __construct()
+	{
+		$this->addTestFileToSuit();
+	}
+	
+	/**
+	 * 添加测试文件到套件中
+	 */
+	public function addTestFileToSuit()
+	{
+		$args = $_SERVER['argv'];
+		unset($args[0], $args[1]);
+		$path = dirname(__FILE__);
+		if (empty($args)) {
+			$this->_addTestFile($path);
+		} else {
+			foreach ($args as $name) {
+				$filePath = $path .  "/" . $name;
+				$this->_addTestFile($filePath);
+			}
+		}
+	}
+	
+	private function _addTestFile($path)
+	{
+		$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+		while ($it->valid()) {
+			if (!$it->isDot())
+			if (!strcmp(substr($it->key(), -8), "Test.php")) {
+				$this->addTestFile($it->key());
+			}
+			$it->next();
+		}		
+	}
+	
+	public static function suite()
+	{
+		return new self();
+	}
+}
